845dda3ae57d7184fac3a04ac0cc739958720c4c
Karan Kute <122804288+kk1701@users.noreply.github.com>
2023-05-16 16:19:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,142 @@
+let ques1 = document.getElementById("ques1");
+let ans1 = document.getElementById("ans1");
+const answer1 = function(){
+    ans1.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque a sapiente quam, ex.";
+    ans1.style.border = "1px solid white";
+    ans1.style.padding = "5px";
+    ans1.style.height = "80px";
+    ans1.style.width = "280px";
+    ans1.style.color = "#e29211";
+    ans1.style.borderRadius = "8px"; 
+}
+ques1.addEventListener("click", answer1);
+
+let ques2 = document.getElementById("ques2");
+let ans2 = document.getElementById("ans2");
+const answer2 = function(){
+    ans2.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque a sapiente quam, ex.";
+    ans2.style.border = "1px solid white";
+    ans2.style.padding = "5px";
+    ans2.style.height = "80px";
+    ans2.style.width = "280px";
+    ans2.style.color = "#e29211";
+    ans2.style.borderRadius = "8px"; 
+}
+ques2.addEventListener("click", answer2);
+
+let ques3 = document.getElementById("ques3");
+let ans3 = document.getElementById("ans3");
+const answer3 = function(){
+    ans3.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque a sapiente quam, ex.";
+    ans3.style.border = "1px solid white";
+    ans3.style.padding = "5px";
+    ans3.style.height = "80px";
+    ans3.style.width = "280px";
+    ans3.style.color = "#e29211";
+    ans3.style.borderRadius = "8px"; 
+}
+ques3.addEventListener("click", answer3);
+
+let ques4 = document.getElementById("ques4");
+let ans4 = document.getElementById("ans4");
+const answer4 = function(){
+    ans4.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque a sapiente quam, ex.";
+    ans4.style.border = "1px solid white";
+    ans4.style.padding = "5px";
+    ans4.style.height = "80px";
+    ans4.style.width = "280px";
+    ans4.style.color = "#e29211";
+    ans4.style.borderRadius = "8px"; 
+}
+ques4.addEventListener("click", answer4);
+
+let ques5 = document.getElementById("ques5");
+let ans5 = document.getElementById("ans5");
+const answer5 = function(){
+    ans5.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque a sapiente quam, ex.";
+    ans5.style.border = "1px solid white";
+    ans5.style.padding = "5px";
+    ans5.style.height = "80px";
+    ans5.style.width = "280px";
+    ans5.style.color = "#e29211";
+    ans5.style.borderRadius = "8px"; 
+}
+ques5.addEventListener("click", answer5);
+
+let ques6 = document.getElementById("ques6");
+let ans6 = document.getElementById("ans6");
+const answer6 = function(){
+    ans6.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque a sapiente quam, ex.";
+    ans6.style.border = "1px solid white";
+    ans6.style.padding = "5px";
+    ans6.style.height = "80px";
+    ans6.style.width = "280px";
+    ans6.style.color = "#e29211";
+    ans6.style.borderRadius = "8px"; 
+}
+ques6.addEventListener("click", answer6);
+
+let ques7 = document.getElementById("ques7");
+let ans7 = document.getElementById("ans7");
+const answer7 = function(){
+    ans7.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque a sapiente quam, ex.";
+    ans7.style.border = "1px solid white";
+    ans7.style.padding = "5px";
+    ans7.style.height = "80px";
+    ans7.style.width = "280px";
+    ans7.style.color = "#e29211";
+    ans7.style.borderRadius = "8px"; 
+}
+ques7.addEventListener("click", answer7);
+
+let ques8 = document.getElementById("ques8");
+let ans8 = document.getElementById("ans8");
+const answer8 = function(){
+    ans8.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque a sapiente quam, ex.";
+    ans8.style.border = "1px solid white";
+    ans8.style.padding = "5px";
+    ans8.style.height = "80px";
+    ans8.style.width = "280px";
+    ans8.style.color = "#e29211";
+    ans8.style.borderRadius = "8px"; 
+}
+ques8.addEventListener("click", answer8);
+
+let ques9 = document.getElementById("ques9");
+let ans9 = document.getElementById("ans9");
+const answer9 = function(){
+    ans9.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque a sapiente quam, ex.";
+    ans9.style.border = "1px solid white";
+    ans9.style.padding = "5px";
+    ans9.style.height = "80px";
+    ans9.style.width = "280px";
+    ans9.style.color = "#e29211";
+    ans9.style.borderRadius = "8px"; 
+}
+ques9.addEventListener("click", answer9);
+
+let ques10 = document.getElementById("ques10");
+let ans10 = document.getElementById("ans10");
+const answer10 = function(){
+    ans10.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque a sapiente quam, ex.";
+    ans10.style.border = "1px solid white";
+    ans10.style.padding = "5px";
+    ans10.style.height = "80px";
+    ans10.style.width = "280px";
+    ans10.style.color = "#e29211";
+    ans10.style.borderRadius = "8px"; 
+}
+ques10.addEventListener("click", answer10);
+
+let ques11 = document.getElementById("ques11");
+let ans11 = document.getElementById("ans11");
+const answer11 = function(){
+    ans11.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque a sapiente quam, ex.";
+    ans11.style.border = "1px solid white";
+    ans11.style.padding = "5px";
+    ans11.style.height = "80px";
+    ans11.style.width = "280px";
+    ans11.style.color = "#e29211";
+    ans11.style.borderRadius = "8px"; 
+}
+ques11.addEventListener("click", answer11);
